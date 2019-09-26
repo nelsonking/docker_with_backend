47f72f4658efbfd0ac973e56b7813c04cd72533b
NelsonKing <gaoyansing@sina.com>
2019-09-26 13:36:01 +0800

    fix

--- install/jenkins.sh
+++ install/jenkins.sh
@@ -5,10 +5,10 @@ source ./common.sh
 check_container jenkins
 
 # 代码共享配置 (需要变更为真实环境)
-jekins_data="/opt/docker_with_backend/jekins/data"
-jekins_data_map="-v $jekins_data:/var/jekins_home"
-jekins_port_map="-p 8080:8080 -p 50000:50000";
+jenkins_data="/opt/docker_with_backend/jenkins/data"
+jenkins_data_map="-v $jenkins_data:/var/jenkins_home"
+jenkins_port_map="-p 8080:8080 -p 50000:50000";
 
 # 安装jenkins
-#docker run -p 8080:8080 -p 50000:50000 -v jenkins_data:/var/jenkins_home jenkinsci/blueocean
-echo $docker_run jekins $jekins_data_map $jekins_port_map $use_network_alias jekins docker.io/jenkins:latest
+#$docker_run jenkins $jenkins_data_map $jekins_port_map $use_network_alias jekins docker.io/jenkins:latest
+$docker_run jenkins $jenkins_data_map $jenkins_port_map $use_network_alias jekins docker.io/jenkins/jenkins:latest
